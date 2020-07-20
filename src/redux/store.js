155d827a6e640d6d991673cab0509feee1fe2be0
Vitaliy <itvkip@yandex.ru>
2020-07-20 17:50:22 +0300

    Added icons for navbar and global store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+let store = {
+	whiteImg = {
+		about: 'about_white.svg',
+		blog: 'blog_white.svg',
+		contact: 'contact_white.svg',
+		home: 'home_white.svg',
+		portfolio: 'portfolio_white.svg',
+		resume: 'resume_white.svg'
+	},
+}
