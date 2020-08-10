e6e318913e0baca946af9c31dffa7feca6bbcc26
Vitaliy <itvkip@yandex.ru>
2020-08-10 12:39:58 +0300

    added content to the Homepage and styles

--- src/components/Content/Home/Home.js
+++ src/components/Content/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import style from './home.module.css';
 
 class Home extends React.Component{
 	render(){
 		return(
-			<h3>This is page Home</h3>
+			<div class={ style.homeTitle }>
+				<div>
+					<h3>Виталий </h3><h3>Кожушко</h3>
+				</div>
+				<p>Frontend developer</p>
+			</div>
 		);
 	}
 }
