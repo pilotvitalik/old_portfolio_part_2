50647a3fd7f29d2f1cdad4c41cf17b0e6e12284a
Vitaliy <itvkip@yandex.ru>
2020-08-14 13:01:55 +0300

    completed making component Contact; creating layout component About

--- src/components/Content/Contact/OtherContacts/Item/Item.js
+++ src/components/Content/Contact/OtherContacts/Item/Item.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import style from './item.module.css';
 
 class Item extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			image: this.props.value.img,
+			alt: this.props.value.titleImg,
+			showTxt: this.props.value.contact,
+		};
+	}
+
 	render(){
 		return(
-			<p>This is Item Component</p>
+			<div className={ style.itemContact }>
+				<img src={ this.state.image } alt={ this.state.alt }/>
+				<p>{ this.state.showTxt }</p>
+			</div>
 		);
 	}
 };
