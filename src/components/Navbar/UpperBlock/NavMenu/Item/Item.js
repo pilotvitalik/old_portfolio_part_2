c671ec6fd03a2dcbcb869f21f7398f49d8b218ff
Vitaliy <itvkip@yandex.ru>
2020-10-29 17:53:27 +0300

    Changed styles for items of Navbar; added dynamic classes for image of list Navbar.

--- src/components/Navbar/UpperBlock/NavMenu/Item/Item.js
+++ src/components/Navbar/UpperBlock/NavMenu/Item/Item.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { NavLink, useRouteMatch } from 'react-router-dom';
+import style from './Item.module.css';
 
 function Item(props){
 	let match = useRouteMatch({
 	  path: props.url,
 	  exact: props.activeOnlyWhenExact
 	});
+	let page = '';
+	let prePage = props.url.replace(/\//g, '');
+	if (prePage.length === 0){
+		prePage = 'mainLength'
+	}
+	page = style[prePage];
 
 	return(
-		<li>
-			<img src={ match ? props.activeImg.whiteImg : props.activeImg.darkImg } alt={ props.data.name }/>		
-			<NavLink exact to={ props.url } activeClassName='activeNavBar'>{ props.data.title }</NavLink>
+		<li className={ page }>	
+			<NavLink exact to={ props.url } activeClassName='activeNavBar'>
+				{ props.data.title }
+				<img src={ match ? props.activeImg.whiteImg : props.activeImg.darkImg } alt={ props.data.name }/>
+			</NavLink>
 		</li>
 	);
 }
